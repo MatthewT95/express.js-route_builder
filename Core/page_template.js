a34a2edfb4028df05457a01966a8d5501428fba8
Matthew Taormina <49125828+MatthewT95@users.noreply.github.com>
2024-02-13 12:22:20 -0500

    Housekeeping

--- Core/page_template.js
+++ Core/page_template.js
@@ -1,25 +1,25 @@
-function response_get = (req,res,next)
+function response_get(req,res,next)
 {
     // Get code
     res.send("Hello world")
 }
 
-function response_post = (req,res,next)
+function response_post(req,res,next)
 {
     // Post code
 }
 
-function response_put= (req,res,next)
+function response_put(req,res,next)
 {
     // Put code
 }
 
-function response_delete = (req,res,next)
+function response_delete(req,res,next)
 {
     // Delete code
 }
 
-module.exports.get=response_get
-module.exports.post=response_post
-module.exports.put=response_put
-module.exports.delete=response_delete
+module.exports.get = response_get
+module.exports.post = response_post
+module.exports.put = response_put
+module.exports.delete = response_delete
