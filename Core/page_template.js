ce96baf3af83e7f3326c0f3fa54549e19ccfe7cf
Matthew Taormina <49125828+MatthewT95@users.noreply.github.com>
2024-02-13 12:00:04 -0500

    Replaced anonymous functions with named functions

--- Core/page_template.js
+++ Core/page_template.js
@@ -1,25 +1,25 @@
-let response_get = (req,res,next)=>
+function get = (req,res,next)=>
 {
     // Get code
     res.send("Hello wolrd")
 }
 
-let response_post = (req,res,next)=>
+function post = (req,res,next)=>
 {
     // Post code
 }
 
-let response_put= (req,res,next)=>
+function put= (req,res,next)=>
 {
     // Put code
 }
 
-let response_delete = (req,res,next)=>
+function delete = (req,res,next)=>
 {
     // Delete code
 }
 
-module.exports.get=response_get
-module.exports.post=response_post
-module.exports.put=response_put
-module.exports.delete=response_delete
+module.exports.get=get
+module.exports.post=post
+module.exports.put=put
+module.exports.delete=delete
