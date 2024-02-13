856050adcf55769b34bdfedb5f5e40d7a7a66802
Matthew Taormina <49125828+MatthewT95@users.noreply.github.com>
2024-02-13 11:40:53 -0500

    Removed unnecessary code

--- Demo-site/index.js
+++ Demo-site/index.js
@@ -3,11 +3,6 @@ const app = express()
 const fs = require('fs');
 let sitemap=JSON.parse(fs.readFileSync("./site-map.json","utf8"))
 
-app.get("/",(req,res) =>
-{
-    res.send("Hello world");
-});
-
 const site_router = require("./sitemap-router")
 
 app.use("/",site_router(sitemap,"./pages/"))
