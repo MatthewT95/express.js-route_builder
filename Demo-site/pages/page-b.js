e881b82d3084550d4fe53a971994460ca7d5a1ab
matthewT95 <matt.taormina@outlook.com>
2024-02-14 09:27:31 -0500

    Updated demo-site with new site template

--- Demo-site/pages/page-b.js
+++ Demo-site/pages/page-b.js
@@ -1,6 +1,25 @@
-let response_get = (req,res)=>
+function response_get(req,res,next)
 {
+    // Get code
     res.send("Page B")
 }
 
-module.exports.get=response_get
+function response_post(req,res,next)
+{
+    // Post code
+}
+
+function response_put(req,res,next)
+{
+    // Put code
+}
+
+function response_delete(req,res,next)
+{
+    // Delete code
+}
+
+module.exports.get = response_get
+module.exports.post = response_post
+module.exports.put = response_put
+module.exports.delete = response_delete
--- Demo-site/pages/pages-root.js
+++ Demo-site/pages/pages-root.js
@@ -1,6 +1,25 @@
-let response_get = (req,res)=>
+function response_get(req,res,next)
 {
-    res.send("Pages Root")
+    // Get code
+    res.send("Pages")
 }
 
-module.exports.get=response_get
+function response_post(req,res,next)
+{
+    // Post code
+}
+
+function response_put(req,res,next)
+{
+    // Put code
+}
+
+function response_delete(req,res,next)
+{
+    // Delete code
+}
+
+module.exports.get = response_get
+module.exports.post = response_post
+module.exports.put = response_put
+module.exports.delete = response_delete
